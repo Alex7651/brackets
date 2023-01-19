fcc483b3f78c54ba8df5d4ddbd57b3bfc3b0ab28
Alex7651 <106837560+Alex7651@users.noreply.github.com>
2023-01-19 22:04:40 +0300

    doc: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  while (
+    str.indexOf("()") !== -1 ||
+    str.indexOf("[]") !== -1 ||
+    str.indexOf("{}") !== -1 ||
+    str.indexOf("||") !== -1 ||
+    str.indexOf("12") !== -1 ||
+    str.indexOf("34") !== -1 ||
+    str.indexOf("56") !== -1 ||
+    str.indexOf("77") !== -1 ||
+    str.indexOf("88") !== -1
+  ) {
+    if (str.indexOf("()") !== -1) {
+      str =
+        str.substring(0, str.indexOf("()")) +
+        str.substring(str.indexOf("()") + 2, str.length);
+    }
+    if (str.indexOf("[]") !== -1) {
+      str =
+        str.substring(0, str.indexOf("[]")) +
+        str.substring(str.indexOf("[]") + 2, str.length);
+    }
+    if (str.indexOf("{}") !== -1) {
+      str =
+        str.substring(0, str.indexOf("{}")) +
+        str.substring(str.indexOf("{}") + 2, str.length);
+    }
+    if (str.indexOf("||") !== -1) {
+      str =
+        str.substring(0, str.indexOf("||")) +
+        str.substring(str.indexOf("||") + 2, str.length);
+    }
+    if (str.indexOf("12") !== -1) {
+      str =
+        str.substring(0, str.indexOf("12")) +
+        str.substring(str.indexOf("12") + 2, str.length);
+    }
+    if (str.indexOf("34") !== -1) {
+      str =
+        str.substring(0, str.indexOf("34")) +
+        str.substring(str.indexOf("34") + 2, str.length);
+    }
+    if (str.indexOf("56") !== -1) {
+      str =
+        str.substring(0, str.indexOf("56")) +
+        str.substring(str.indexOf("56") + 2, str.length);
+    }
+    if (str.indexOf("77") !== -1) {
+      str =
+        str.substring(0, str.indexOf("77")) +
+        str.substring(str.indexOf("77") + 2, str.length);
+    }
+    if (str.indexOf("88") !== -1) {
+      str =
+        str.substring(0, str.indexOf("88")) +
+        str.substring(str.indexOf("88") + 2, str.length);
+    }
+  }
+  if (str) {
+    return false;
+  } else {
+    return true;
+  }
+};
